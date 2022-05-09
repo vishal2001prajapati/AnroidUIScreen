e2d3cf11c523ee3e9faf3e00923463c8f98cc27f
vishal prajapati <vishal.prajapati@simformsolutions.com>
2022-05-09 19:48:00 +0530

    TE5-T884 signIn Screen Complete

--- app/src/main/java/com/example/anroiduiscreen/signupscreen/SignUpActivity.kt
+++ app/src/main/java/com/example/anroiduiscreen/signupscreen/SignUpActivity.kt
@@ -1,11 +1,12 @@
-package com.example.anroiduiscreen
+package com.example.anroiduiscreen.signupscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.anroiduiscreen.R
 
-class MainActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 }
